f80828012646a538d3ff3613442484dc85f7bf51
Альбина Архипова <alba91@inbox.ru>
2024-01-17 00:24:42 +0300

    Adds files

--- js/main.js
+++ js/main.js
@@ -1,73 +1,313 @@
-'use strict';
+const openMenuButton = document.querySelector('.header__open-menu');
+const closeMenuButton = document.querySelector('.header__close-menu');
+const nav = document.querySelector('.header__nav');
 
-function addMask(currentTelInput) {
-  window.iMaskJS(currentTelInput, {mask: '{8} (000) 000 00 00'});
+if (openMenuButton) {
+  openMenuButton.addEventListener('click', function () {
+    nav.classList.add('active');
+    openMenuButton.style.display = 'none';
+  })
 }
 
-//мобильное меню
-(function () {
-  const navToggleButton = document.querySelector(`.js_toggle-nav`);
-  const headerNav = document.querySelector(`.page-header__nav`);
+if (closeMenuButton) {
+  closeMenuButton.addEventListener('click', function () {
+    nav.classList.remove('active');
+    openMenuButton.removeAttribute('style');
+  })
+}
 
-  if (navToggleButton) {
-    navToggleButton.addEventListener(`click`, function (evt) {
-      evt.preventDefault();
-      headerNav.classList.toggle(`page-header__nav--opened`)
-    })
-  }
-})();
+const menuItems = document.querySelectorAll('.header__nav-item');
+const menuItemsLinks = document.querySelectorAll('.header__nav-item a[href]');
 
-//плавный скролл
-(function () {
-  const nav = document.querySelector(`.page-header__nav`);
-  const navLinks = document.querySelectorAll(`.page-header__nav-item a`);
-  if (navLinks.length > 0) {
-    Array.from(navLinks).forEach(link => {
-      link.addEventListener('click', function(evt) {
-          evt.preventDefault();
-          if (document.body.clientWidth < '767') {
-            nav.classList.remove(`page-header__nav--opened`);
-          }
-          let href = this.getAttribute('href').substring(1);
-          const scrollTarget = document.getElementById(href);
-          const topOffset = document.querySelector('.page-header').offsetHeight;
-          const elementPosition = scrollTarget.getBoundingClientRect().top;
-          const offsetPosition = elementPosition - topOffset;
-          window.scrollBy({
-              top: offsetPosition,
-              behavior: 'smooth'
-          });
-      });
-    });
-  }
-})();
+if (menuItems.length > 0) {
+  menuItems.forEach((menuItem) => {
 
-//блок Часто задаваемые вопросы
-(function () {
-  const questions = document.querySelectorAll(`.faq__list-question`);
+    const subnav = menuItem.querySelector('.header__subnav-list');
 
-  if (questions.length > 0) {
-    let currentOpen = ``;
-    Array.from(questions).forEach(question => {
-      question.addEventListener(`click`, function (evt) {
-        evt.preventDefault();
-        if (currentOpen && currentOpen !== question.closest(`li`)) {
-          currentOpen.classList.remove(`active`);
-        }
-
-        question.closest(`li`).classList.toggle(`active`);
-        currentOpen = question.closest(`li`);
+    if (subnav) {
+      menuItem.addEventListener('click', function() {
+        menuItem.classList.toggle('active');
       })
-    })
-  }
-})();
+    }
+  })
+}
 
-// //маски на тел
-// (function () {
-//   const telInputs = document.querySelectorAll(`input[type="tel"]`);
-//   if (telInputs.length > 0) {
-//     Array.from(telInputs).forEach(telInput => {
-//       addMask(telInput);
-//     })
-//   }
-// })();
+if (menuItemsLinks.length > 0) {
+  menuItemsLinks.forEach((menuItemLink) => {
+    menuItemLink.addEventListener('click', function () {
+      nav.classList.remove('active');
+      openMenuButton.removeAttribute('style');
+    })
+  })
+}
+
+
+const cigarLink = document.querySelector('.menu__link--cigar');
+const wineLink = document.querySelector('.menu__link--wine');
+
+if (cigarLink && wineLink) {
+  cigarLink.addEventListener('mouseover', function () {
+    wineLink.classList.add('blur');
+  })
+
+  cigarLink.addEventListener('mouseout', function () {
+    wineLink.classList.remove('blur');
+  })
+
+  wineLink.addEventListener('mouseover', function () {
+    cigarLink.classList.add('blur');
+  })
+
+  wineLink.addEventListener('mouseout', function () {
+    cigarLink.classList.remove('blur');
+  })
+}
+
+
+//наползающий фон
+
+const slippingBg = document.querySelector('.header__slipping-bg');
+ if (slippingBg) {
+
+  window.addEventListener('scroll', function() {
+    const curentPageOffset = pageYOffset;
+
+    if (pageYOffset > 1) {
+
+      slippingBg.style.bottom = '-' + (69 - (curentPageOffset / 4)) + '%';
+    }
+  });
+ }
+
+
+ //осветление меню
+
+const darkMenuImg = document.querySelector('.menu__dark-img');
+const menuLinksWrapper = document.querySelector('.menu__links');
+
+if (darkMenuImg && menuLinksWrapper) {
+
+  let top = menuLinksWrapper.getBoundingClientRect().top + window.scrollY - document.documentElement.clientHeight / 2;
+
+    // if (document.body.clientWidth < '768') {
+    //   top = section.getBoundingClientRect().top + window.scrollY - document.documentElement.clientHeight / 1.5;
+    // }
+
+  if (pageYOffset >= top) {
+    darkMenuImg.style.animationName = 'smooth';
+    menuLinksWrapper.classList.add('hidden-bg');
+  }
+
+  window.addEventListener('scroll', function () {
+
+    if (pageYOffset >= top) {
+      darkMenuImg.style.animationName = 'smooth';
+      menuLinksWrapper.classList.add('hidden-bg');
+    } else {
+      darkMenuImg.removeAttribute('style');
+      menuLinksWrapper.classList.remove('hidden-bg');
+    }
+  })
+}
+
+
+//Меню
+const menuSection = document.querySelector('.menu');
+const cigarMenuLinks = menuSection.querySelectorAll('[data-link="cigar"]');
+const wineMenuLinks = menuSection.querySelectorAll('[data-link="wine"]');
+const headerMenuLinks = menuSection.querySelectorAll('.section-header__links a');
+const headerCigarLink = menuSection.querySelector('.section-header__links [data-link="cigar"]');
+const headerWineLink = menuSection.querySelector('.section-header__links [data-link="wine"]');
+const menuLinks = menuSection.querySelector('.menu__links');
+const cigarMenu = menuSection.querySelector('.menu__part--cigar');
+const wineMenu = menuSection.querySelector('.menu__part--wine');
+
+const backLinks = menuSection.querySelectorAll('.menu__back-link');
+if (backLinks.length > 0) {
+  backLinks.forEach((backLink) => {
+    backLink.addEventListener('click', function () {
+
+      currentOpenMenu.classList.remove('visible');
+      menuLinks.classList.remove('hidden');
+
+      currentOpenMenu = '';
+
+      if (headerMenuLinks) {
+        headerMenuLinks.forEach((link)=> {
+          link.classList.remove('active');
+        })
+      }
+    })
+  })
+}
+
+let currentOpenMenu = '';
+
+
+if (wineMenuLinks.length > 0) {
+  wineMenuLinks.forEach((link) => {
+    link.addEventListener('click', function () {
+
+      menuLinks.classList.add('hidden');
+      wineMenu.classList.add('visible');
+
+
+      if (currentOpenMenu && currentOpenMenu != wineMenu) {
+        currentOpenMenu.classList.remove('visible');
+      }
+
+      currentOpenMenu = wineMenu;
+
+      headerCigarLink.classList.remove('active');
+      headerWineLink.classList.add('active');
+    })
+  })
+}
+
+if (cigarMenuLinks.length > 0) {
+  cigarMenuLinks.forEach((link) => {
+    link.addEventListener('click', function () {
+
+      menuLinks.classList.add('hidden');
+      cigarMenu.classList.add('visible');
+
+      if (currentOpenMenu && currentOpenMenu != cigarMenu) {
+        currentOpenMenu.classList.remove('visible');
+      }
+
+      currentOpenMenu = cigarMenu;
+
+      headerWineLink.classList.remove('active');
+      headerCigarLink.classList.add('active');
+    })
+  })
+}
+
+
+//фильтр
+const filterOpenButtons = document.querySelectorAll('.js_open-filter');
+
+if (filterOpenButtons.length > 0) {
+
+  document.addEventListener('click', function (evt) {
+    filterOpenButtons.forEach((button) => {
+      if (evt.target !== button) {
+        button.parentElement.classList.remove('active');
+      }
+    })
+  });
+
+  filterOpenButtons.forEach((button) => {
+
+    const currentfilter = button.parentElement;
+      // const options = currentfilter.querySelectorAll('span');
+      // options.forEach((option) => {
+      //   option.addEventListener('click', function () {
+
+      //     const optionText = option.textContent;
+      //     button.textContent = optionText;
+      //   });
+      // })
+
+
+    button.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      const currentfilter = button.parentElement;
+      currentfilter.classList.toggle('active');
+    })
+  })
+}
+
+
+// слайдер
+const interiorSection = document.querySelector('.interior');
+const interiorSliderWrapper = interiorSection.querySelector('.interior__slider-wrapper');
+const interiorSliderContainer = interiorSection.querySelector('.interior__slider-container');
+
+if (interiorSliderContainer) {
+  const sliderNextButton = interiorSliderWrapper.querySelector('.slider-btn--next');
+  const sliderPrevButton = interiorSliderWrapper.querySelector('.slider-btn--prev');
+
+  const swiper = new Swiper (interiorSliderContainer, {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    direction: 'horizontal',
+    //effect: "fade",
+
+    navigation: {
+      nextEl: sliderNextButton,
+      prevEl: sliderPrevButton,
+    },
+
+    // autoplay: {
+    //   delay: 3500,
+    //   disableOnInteraction: true,
+    // },
+  });
+
+  const actualSections = interiorSection.querySelectorAll('[data-section-show]');
+
+  const headerMenuLinks = interiorSection.querySelectorAll('.section-header__links a');
+
+  function showActiveElements () {
+    const activeSlide = interiorSection.querySelector('.swiper-slide-active');
+    const dataValue = activeSlide.dataset.section;
+
+    const actualText = document.querySelector('[data-section-show="' + dataValue + '"]');
+    actualText.style.display="block";
+
+    const currentLink = document.querySelector('[data-link="' + dataValue + '"]');
+    currentLink.classList.add('active');
+  }
+
+  showActiveElements();
+
+  function showNextSlidesPreview () {
+    const nextSlidePreview = interiorSection.querySelector('.interior__next-slide-preview');
+    const nextSlide = interiorSection.querySelector('.swiper-slide-next');
+
+    if (nextSlide) {
+      const nextSlideImg = nextSlide.querySelector('img');
+      const nextSlideImgPath = nextSlideImg.getAttribute('src');
+
+      nextSlidePreview.style.backgroundImage=`url(${nextSlideImgPath})`;
+    } else {
+      nextSlidePreview.style.backgroundImage='none';
+    }
+
+    const afterNextSlidePreview = interiorSection.querySelector('.interior__after-next-slide-preview');
+
+    if (nextSlide) {
+      const afterNextSlide = nextSlide.nextElementSibling;
+
+      if (afterNextSlide) {
+        const afterNextSlideImg = afterNextSlide.querySelector('img');
+        const afterNextSlideImgPath = afterNextSlideImg.getAttribute('src');
+
+        afterNextSlidePreview.style.backgroundImage=`url(${afterNextSlideImgPath})`;
+      } else {
+        afterNextSlidePreview.style.backgroundImage='none';
+      }
+    }
+  }
+
+  showNextSlidesPreview();
+
+  swiper.on('slideChangeTransitionEnd', function () {
+    if (actualSections) {
+      actualSections.forEach((section) => {
+        section.style.display="none";
+      })
+    }
+
+    if (headerMenuLinks) {
+      headerMenuLinks.forEach((link)=> {
+        link.classList.remove('active');
+      })
+    }
+
+    showActiveElements();
+
+    showNextSlidesPreview();
+  })
+}
